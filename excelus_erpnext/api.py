b2e6754a290efda0202dd4c898c357a2f173ed01
Gaurav Naik <gvrvnk@gmail.com>
2016-11-14 18:45:22 +0530

    Updated BOM Calculation logic.

--- excelus_erpnext/api.py
+++ excelus_erpnext/api.py
@@ -1,37 +1,86 @@
+import frappe
+from frappe import _
+import json
 
-def calculate_film_qty_per_kg(item_code):
-	pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
-	packs_per_kg = 1000/pack_weight
+#Pack weight comes from bom.item
+
+def calculate_film_qty_per_kg(pack_weight, item_code):
+	#pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
+	packs_per_kg_fg = 1000/pack_weight
 	pack_width = frappe.db.get_value("Item", item_code, "excelus_pm_width")
 	pack_length = frappe.db.get_value("Item", item_code, "excelus_pm_length")
 	film_wastage = frappe.db.get_value("Item", item_code, "excelus_pm_wastage")
-	thickness = frappe.db.get_value("Item", item_code, "excelus_pm_thickness")
+	avg_film_thickness = frappe.db.get_value("Item", item_code, "excelus_pm_thickness")
 
-	film_per_pack = ((pack_length * pack_width)/(1 / film_wastage)) / (10**6)
-	film_per_kg = film_per_pack * packs_per_kg
+	film_per_pack = ((pack_length * pack_width)/(1-(film_wastage/100))) / (10**6)
+	fg_film_per_kg = film_per_pack * packs_per_kg_fg
 
-	return film_per_kg/(10*3)
+	return (avg_film_thickness * fg_film_per_kg)/(10**3)
 
-def calculate_carton_qty_per_kg(item_code):
-	pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
-	packs_per_carton = frappe.db.get_value("Item", item_code, "packs_per_carton")
+def calculate_carton_qty_per_kg(pack_weight, item_code):
+	#pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
+	packs_per_carton = frappe.db.get_value("Item", item_code, "excelus_packs_per_carton")
 	carton_height =  frappe.db.get_value("Item", item_code, "excelus_pm_height")
 	carton_width =  frappe.db.get_value("Item", item_code, "excelus_pm_width")
 	carton_length =  frappe.db.get_value("Item", item_code, "excelus_pm_length")	
 	carton_board_area = ((carton_height * carton_length)*2 + (carton_length * carton_width)*2 + (carton_width * carton_height)*2) / (10**6)
-	carton_thickness = frappe.db.get_value("Item", item_code, "excelus_pm_thickness")
-	carton_weight = (carton_board_area * carton_thickness) / (10**3)
+	carton_gsm = frappe.db.get_value("Item", item_code, "excelus_pm_thickness")
+	carton_weight = (carton_board_area * carton_gsm) / (10**3)
 	fg_weight_per_carton = (pack_weight * packs_per_carton) / (10**3)
-	carton_per_kg = 1/fg_weight_per_carton
+	fg_carton_per_kg = 1/fg_weight_per_carton
 		
-	return ((carton_per_kg * fg_weight_per_carton) / (10**3))
+	#return ((fg_carton_per_kg * fg_weight_per_carton) / (10**3)), packs_per_carton, carton_width
+	return (fg_carton_per_kg * fg_weight_per_carton), packs_per_carton, carton_width
 
-def calculate_tape_qty_per_kg(item_code, packs_per_carton, carton_width):
+def calculate_tape_qty_per_kg(pack_weight, item_code, packs_per_carton, carton_width):
+	#pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
+	roll_width =  frappe.db.get_value("Item", item_code, "excelus_pm_width")
+	tape_gsm = frappe.db.get_value("Item", item_code, "excelus_pm_thickness")
+	fg_weight_per_carton  = (pack_weight * packs_per_carton) / (10**3)
+	tape_reqd_per_carton = ((carton_width*2)+(80*2)) / (10**3)
+	fg_tape_reqd_per_kg = tape_reqd_per_carton / fg_weight_per_carton
+
+	return (fg_tape_reqd_per_kg * tape_gsm) / (10**3)
+
+@frappe.whitelist()
+def calculate_pm_qtys(item_code, items):
+
+	pack_weight = frappe.db.get_value("Item", item_code, "net_weight")
+
+	repeating_items = []
+
+	items = json.loads(items)
+
+	for item in items:
+		item.update({"item_group": frappe.db.get_value("Item", item.get("item_code"), "item_group")})
+		if process_item_group(item.get("item_group")) in ["pm-tape", "pm-laminate", "pm-carton"]:
+			repeating_items.append(item.get("item_group"))
+
+	distinct_items = set(repeating_items)
+
+	for x in xrange(1, 10):
+		print len(repeating_items), len(distinct_items)
+
+	if len(repeating_items)!= len(distinct_items):
+		frappe.throw(_("Same Item has been entered more than once."))
 
 
-def calculate_pm_qtys(item_code):
-	film_qty_per_kg = calculate_film_qty_per_kg(item_code)
-	carton_qty_per_kg = calculate_carton_qty_per_kg(item_code)
-	tape_qty_per_kg = calculate_tape_qty_per_kg(item_code)
+	film = [x.get("item_code") for x in items if process_item_group(x.get("item_group")) == "pm-laminate"]
+	film_qty_per_kg = calculate_film_qty_per_kg(pack_weight, film[0])
+	
+	carton = [x.get("item_code") for x in items if process_item_group(x.get("item_group")) == "pm-carton"]
+	carton_qty_per_kg, packs_per_carton, carton_width = calculate_carton_qty_per_kg(pack_weight, carton[0])
 
-	return frappe.dict({ "film_qty": film_qty_per_kg, "carton_qty": carton_qty_per_kg, "tape_qty": tape_qty_per_kg })
+	tape = [x.get("item_code") for x in items if process_item_group(x.get("item_group")) == "pm-tape"]
+	tape_qty_per_kg = calculate_tape_qty_per_kg(pack_weight,  tape[0], packs_per_carton, carton_width)
+
+
+ 	for x in xrange(1,10):
+ 		print packs_per_carton, carton_width
+		print frappe._dict({ "laminate_qty": film_qty_per_kg, "carton_qty": carton_qty_per_kg, "tape_qty": tape_qty_per_kg })
+
+	return frappe._dict({ "laminate_qty": film_qty_per_kg, "carton_qty": carton_qty_per_kg, "tape_qty": tape_qty_per_kg })
+
+
+def process_item_group(item_group):
+	return item_group.replace(" ", "").lower()
