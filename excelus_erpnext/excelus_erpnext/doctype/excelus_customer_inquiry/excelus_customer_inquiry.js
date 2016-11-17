cdbeb4770e83d0c61306d8a6130679396d1abbcf
Bhamesh Kore <bhams91.kore@gmail.com>
2016-11-17 12:29:19 +0530

    In Requirement child table, Item dropdown should only show finished goods.

--- excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.js
+++ excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.js
@@ -3,6 +3,12 @@
 
 frappe.ui.form.on('Excelus Customer Inquiry', {
 	refresh: function(frm) {
-
+		cur_frm.set_query("item", "ci_requirements", function() {
+        return {
+           filters: [
+				['Item', 'item_group', '=', 'Products']
+		   ]
+        };
+    });
 	}
 });
