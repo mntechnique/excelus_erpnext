f09f7e968f9dbf2a1d6217d5d82db43b095198a9
Vishal <vishaldhayagude07@gmail.com>
2016-11-18 18:50:52 +0530

    [new]CI item qty calculation partially done

--- excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.js
+++ excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.js
@@ -4,11 +4,30 @@
 frappe.ui.form.on('Excelus Customer Inquiry', {
 	refresh: function(frm) {
 		cur_frm.set_query("item", "ci_requirements", function() {
-        return {
-           filters: [
-				['Item', 'item_group', '=', 'Products']
-		   ]
-        };
-    });
+            return {
+               filters: [
+    				['Item', 'item_group', '=', 'Products']
+    		   ]
+            };
+        });
+        frm.add_custom_button(__("Fetch Items"), function(){
+            /*var req_items = [];
+            var req = frm.doc.ci_requirements;
+            for (var i = req.length - 1; i >= 0; i--) {
+                req_items.push(req[i].item);
+            }*/
+            frappe.call({
+                method: "excelus_erpnext.excelus_erpnext.doctype.excelus_customer_inquiry.excelus_customer_inquiry.calculate_fetch_item",
+                args: {
+                    "req_items": frm.doc.ci_requirements
+                },
+                callback: function(r){
+                    // msgprint(r.message);
+                   console.log(r.message);
+                }
+            });
+        });
 	}
 });
+
+cur_frm.add_fetch("item" ,"default_bom","bom")
