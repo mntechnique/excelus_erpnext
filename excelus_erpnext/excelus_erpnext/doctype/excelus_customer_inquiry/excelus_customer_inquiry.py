d0b0c1eea37ac2d573b96cd5aaafa50f63f2c54a
Vishal <vishaldhayagude07@gmail.com>
2016-11-21 13:50:26 +0530

    [mod]excelus CI modified

--- excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.py
+++ excelus_erpnext/excelus_erpnext/doctype/excelus_customer_inquiry/excelus_customer_inquiry.py
@@ -13,23 +13,19 @@ class ExcelusCustomerInquiry(Document):
 
 @frappe.whitelist()
 def calculate_fetch_item(req_items):
+    #final_item list created for result to fetch into ci_items table
     final_items = []
     import json
-
+    #req_items_js load ci_requiremnt input like item_code, qty and bom list.
     req_items_js = json.loads(req_items)
-
-
-
-
-
     for item in req_items_js:
-        for x in xrange(1,10):
-            print item
+        # throws error message if default bom not selected for item.
         if not item.get('bom'):
             frappe.throw(_("Please select BOM for all requirement Items"))
         bom_items = frappe.db.get_all("BOM Item", filters={"parent":item.get('bom')}, fields=["*"])
-        for bom_item in bom_items:
 
+        #Calculate (bom item qty / bom qty) * input ci_requirment qty. Addition does becasue of duplicate item in bom.
+        for bom_item in bom_items:
             found_item = [x for x in final_items if x.item_code==bom_item.item_code]
             qty_ratio = bom_item.qty / frappe.db.get_value("BOM", item.get("bom"), "quantity")
             if len(found_item)==1:
