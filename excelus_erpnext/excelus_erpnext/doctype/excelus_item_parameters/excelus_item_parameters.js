3226f27520d13b7625fe8cb6a0d1375363455afb
Gaurav Naik <gvrvnk@gmail.com>
2017-04-08 18:06:11 +0530

    [New] Separate doctype for item parameters

---
+++ excelus_erpnext/excelus_erpnext/doctype/excelus_item_parameters/excelus_item_parameters.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, MN Technique and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Excelus Item Parameters', {
+	refresh: function(frm) {
+
+	}
+});
